3e37742d2aae0ba005b262133b1440e44efdb346
Sinri Edogawa <e.joshua.s.e@gmail.com>
2019-11-28 13:49:38 +0800

    将文件型autoload标记为准备废弃

--- autoload.php
+++ autoload.php
@@ -4,12 +4,16 @@
  * User: Sinri
  * Date: 2017/9/30
  * Time: 14:08
+ *
+ * @deprecated since 0.2
  */
+
+use sinri\sinridingtalkhelper\ApiHelper\SinriDingtalkHelper;
 
 require_once __DIR__ . '/ApiHelper/SinriDingtalkHelper.php';
 
 spl_autoload_register(function ($class_name) {
-    $file_path = \sinri\sinridingtalkhelper\ApiHelper\SinriDingtalkHelper::getFilePathOfClassNameWithPSR0(
+    $file_path = SinriDingtalkHelper::getFilePathOfClassNameWithPSR0(
         $class_name,
         'sinri\sinridingtalkhelper',
         __DIR__,
